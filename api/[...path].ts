d990f2091d6395dba4d4c6a07b0d002d1c889840
Sai Aishwarya <146462239+aish-am22@users.noreply.github.com>
2026-05-30 08:11:28 +0000

    fix: explicit server path

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,3 +1,8 @@
-import { app } from "../server";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const serverBundle = require("../dist/server.cjs");
+
+const app = serverBundle.default || serverBundle;
 
 export default app;
